753611edf86206a4c2ca325803da2b47a0f7a544
ankitsunil530 <22bcs244@iiitdmj.ac.in>
2026-05-01 16:40:00 +0530

    frontend complete

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://fenmo-backend-p4u7.onrender.com",
 });
 
 export const getExpenses = (params) => API.get("/expenses", { params });
